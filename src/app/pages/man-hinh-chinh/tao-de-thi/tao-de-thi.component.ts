110ce574d282bf376f320fa09a2cbbaa01809f8f
minhducdma <minhducdma@gmail.com>
2021-08-14 04:10:58 +0700

    .

--- src/app/pages/man-hinh-chinh/tao-de-thi/tao-de-thi.component.ts
+++ src/app/pages/man-hinh-chinh/tao-de-thi/tao-de-thi.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ICauHoi, IDapAn } from '../model/tao-de-thi.model';
 import '../../../shared/controls/ckeditor-config/ckeditor.loader';
 import 'ckeditor';
-import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 @Component({
     selector: 'ngx-tao-de-thi',
     templateUrl: './tao-de-thi.component.html',
@@ -46,15 +46,15 @@ export class TaoDeThiComponent implements OnInit {
         }
     }
 
-    saveListCauHoi(){
-        console.log(this.dataCauHoi);
+    saveListCauHoi() {
+        //console.log(this.flatTreeArray(this.dataCauHoi));
     }
 
-    showCauHoi(item){
+    showCauHoi(item) {
         this.selectedCauHoi = item as ICauHoi;
     }
 
-    addCauTraLoi(item: ICauHoi){
+    addCauTraLoi(item: ICauHoi) {
         let dapAn = {
             cauHoiId: item.id,
             noiDungCauHoi: null,
@@ -66,7 +66,7 @@ export class TaoDeThiComponent implements OnInit {
         this.selectedCauHoi.dapAns.push(dapAn);
         console.log(this.selectedCauHoi);
     }
-    removeCauTraLoi(item: IDapAn){
+    removeCauTraLoi(item: IDapAn) {
         const index = this.selectedCauHoi.dapAns.indexOf(item, 0);
         if (index > -1) {
             this.selectedCauHoi.dapAns.splice(index, 1);
@@ -75,12 +75,38 @@ export class TaoDeThiComponent implements OnInit {
 
     drop(event: CdkDragDrop<string[]>) {
         if (event.previousContainer === event.container) {
-          moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+            moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
         } else {
-          transferArrayItem(event.previousContainer.data,
-                            event.container.data,
-                            event.previousIndex,
-                            event.currentIndex);
+            transferArrayItem(event.previousContainer.data,
+                event.container.data,
+                event.previousIndex,
+                event.currentIndex);
         }
-      }
+    }
+
+    
+    // flatTreeArray(root) {
+    //     var stack = [], array = [], hashMap = {};
+    //     stack.push(root);
+    
+    //     while(stack.length !== 0) {
+    //         var node = stack.pop();
+    //         if(node.children === null) {
+    //             this.visitNode(node, hashMap, array);
+    //         } else {
+    //             for(var i = node.children.length - 1; i >= 0; i--) {
+    //                 stack.push(node.children[i]);
+    //             }
+    //         }
+    //     }
+    
+    //     return array;
+    // }
+
+    // visitNode(node, hashMap, array) {
+    //     if(!hashMap[node.data]) {
+    //         hashMap[node.data] = true;
+    //         array.push(node);
+    //     }
+    // }
 }
