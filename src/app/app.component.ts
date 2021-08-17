a979829ba90f90373dee1bd39e57cf9c08cb6808
minhducdma <minhducdma@gmail.com>
2021-08-17 15:09:49 +0700

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,39 +9,40 @@ import { SeoService } from './@core/utils/seo.service';
 
 import { AuthService } from '@abp/ng.core';
 import { OAuthService } from 'angular-oauth2-oidc'
-import { NbMenuService} from '@nebular/theme';
+import { NbMenuService } from '@nebular/theme';
 
 
 @Component({
-  selector: 'ngx-app',
-  template: '<router-outlet></router-outlet>',
+	selector: 'ngx-app',
+	template: '<router-outlet></router-outlet>',
 })
 export class AppComponent implements OnInit {
 
-  constructor(private analytics: AnalyticsService, private seoService: SeoService,private authService: AuthService,
-    private oAuthService: OAuthService, private menuService : NbMenuService) {
-  }
+	constructor(private analytics: AnalyticsService, private seoService: SeoService, private authService: AuthService,
+		private oAuthService: OAuthService, private menuService: NbMenuService) {
+	}
 
-  ngOnInit(): void {
-    this.checkLogin();
-    this.menuService.onItemClick()
-      .subscribe((event) => {
-        this.onContecxtItemSelection(event.item.title);
-      });
-    this.analytics.trackPageViews();
-    this.seoService.trackCanonicalChanges();
-  }
+	ngOnInit(): void {
+		this.checkLogin();
+		this.menuService.onItemClick()
+			.subscribe((event) => {
+				this.onContecxtItemSelection(event.item.title);
+			});
+		this.analytics.trackPageViews();
+		this.seoService.trackCanonicalChanges();
+	}
 
-  checkLogin(): void{
-    if(!this.oAuthService.hasValidAccessToken()){
-      console.log(1);
-      this.authService.navigateToLogin();
-    }
+	checkLogin(): void {
+		if (!this.oAuthService.hasValidAccessToken()) {
+			console.log(1);
+			this.authService.navigateToLogin();
+		}
 
-  }
-  onContecxtItemSelection(title) {
-    if(title == 'Log out')
-      this.authService.logout();
-      this.authService.navigateToLogin();
-  }
+	}
+	onContecxtItemSelection(title) {
+		if (title == 'Log out') {
+			this.authService.logout();
+			this.authService.navigateToLogin();
+		}
+	}
 }
