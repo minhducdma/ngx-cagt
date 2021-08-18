3a558f56d605a7a5fd05fb738611fec549c1aff8
minhducdma <minhducdma@gmail.com>
2021-08-18 22:01:07 +0700

    .

--- src/app/shared/controls/cagt-select/cagt-select.module.ts
+++ src/app/shared/controls/cagt-select/cagt-select.module.ts
@@ -2,17 +2,13 @@ import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CagtSelectComponent } from './cagt-select.component';
 import { NbSelectModule } from '@nebular/theme';
-import {MatSelectModule} from '@angular/material/select';
-
-
 
 @NgModule({
     declarations: [CagtSelectComponent],
     entryComponents: [CagtSelectComponent],
     imports: [
         CommonModule,
-        NbSelectModule,
-        MatSelectModule
+        NbSelectModule
     ],
     exports: [
         CagtSelectComponent,
