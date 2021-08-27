a6c8ba6d2005325d11b9716341c0c13fdbe8fceb
minhducdma <minhducdma@gmail.com>
2021-08-28 01:52:28 +0700

    .

---
+++ src/app/pages/miscellaneous/successful-test/successful-test.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-successful-test',
+  templateUrl: './successful-test.component.html',
+  styleUrls: ['./successful-test.component.scss']
+})
+export class SuccessfulTestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
