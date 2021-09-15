d624475e2cc9414d27246005da654ea0500652b6
minhducdma <minhducdma@gmail.com>
2021-09-16 00:17:17 +0700

    .

--- src/app/@theme/components/header/header.component.ts
+++ src/app/@theme/components/header/header.component.ts
@@ -57,6 +57,8 @@ export class HeaderComponent implements OnInit, OnDestroy {
     if(this.oAuthService.hasValidAccessToken()){
       this.config.getOne$("currentUser").subscribe(currentUser => {
         this.user = currentUser;
+        if(localStorage.getItem('currentUser') == null)
+          localStorage.setItem('currentUser', currentUser);
      })
 
     }
--- src/app/shared/base/base-check-permission.ts
+++ src/app/shared/base/base-check-permission.ts
@@ -1,17 +1,19 @@
 import { Directive, Injector, OnDestroy, OnInit } from "@angular/core";
 import { Subject } from "rxjs";
 import { IUser } from "../models/user.model";
+import { ConfigStateService } from '@abp/ng.core';
+import { OAuthService } from 'angular-oauth2-oidc'
 
 @Directive()
 export abstract class BaseCheckPermissionComponent implements OnInit, OnDestroy {
     protected destroyed$ = new Subject();
-    currentUser: IUser = null; 
+    currentUser: IUser = null;
 
     constructor(
-        injector: Injector
-    ){}
+        injector: Injector,
+    ) { }
 
-    ngOnInit(){
+    ngOnInit() {
         this.getCurrentUser();
     }
 
@@ -20,7 +22,7 @@ export abstract class BaseCheckPermissionComponent implements OnInit, OnDestroy 
         this.destroyed$.complete();
     }
 
-    getCurrentUser(){
-        this.currentUser = JSON.parse(localStorage.getItem("currentUser")) as IUser
+    getCurrentUser() {
+        this.currentUser = JSON.parse(localStorage.getItem("currentUser")) as IUser;
     }
 }
